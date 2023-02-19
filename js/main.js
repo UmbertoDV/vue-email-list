cd0f4a12be304ed130b5abc3c034b45a5bed3f85
UmbertoDV <biccoumby2002@hotmail.com>
2023-02-19 11:28:27 +0100

    es with no bonus

--- js/main.js
+++ js/main.js
@@ -3,7 +3,23 @@ const { createApp } = Vue;
 createApp({
 	data() {
 		return {
-			message: "Hello Vue!",
+			randomMailsList: [],
 		};
+	},
+	methods: {
+		getRandomMail() {
+			for (i = 0; i < 10; i++) {
+				axios
+					.get("https://flynn.boolean.careers/exercises/api/random/mail")
+					.then((response) => {
+						if (this.randomMailsList.length < 10) {
+							this.randomMailsList.push(response.data.response);
+						}
+					});
+			}
+		},
+	},
+	created() {
+		this.getRandomMail();
 	},
 }).mount("#app");
